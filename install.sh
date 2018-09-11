f346bfdc6ebd3267fd8e3e5363466075b11466f1
jsmith <jasmith79@gmail.com>
2018-09-11 16:46:51 -0400

    WIP

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/bash
 
-if [[ $(uname) =~ [Dd]arwin ]]; then
+# Determine operating system
+os=$(uname)
+echo "Operating System: $os"
+
+# Determine current user
+user=$(logname)
+who=$(whoami)
+if [[ $user = "" ]]; then
+	user="$SUDO_USER"
+fi
+
+if [[ $user = "" ]]; then
+	user="$who"
+fi
+
+echo "Script called by user $user."
+
+if [[ "$os" =~ [Dd]arwin ]]; then
   echo "On OS X"
   # NOTE: the rest of this assumes that you have the XCode CLI tools installed.
 
@@ -25,40 +42,48 @@ if [[ $(uname) =~ [Dd]arwin ]]; then
   npm install -g webpack-dev-server
   echo "Done."
 
-elif [[ $(uname) =~ [Ll]inux ]]
+elif [[ "$os" =~ [Ll]inux ]]
 then
   # For now, we're assuming "Linux" means Ubuntu or Mint
   # TODO: rework for yum/pacman later.
-  ubuntu_version=0
+  ubuntu_version=false
   distro=$(cat /etc/lsb-release | head -n 1 | grep -o "=[a-zA-Z]\+" | cut -c2-)
   if [ $distro == "LinuxMint" ]; then
-    ubuntu_version=$(cat /etc/upstream-release/lsb-release | grep -o "[0-9]\{2\}\.[0-9]\{2\}")
+    ubuntu_version=$(cat /etc/upstream-release/lsb-release | grep -o "[0-9]\{2\}" | head -n 1)
   elif [ $distro == "Ubuntu" ]
   then
-    ubuntu_version=$(cat /etc/lsb-release | grep -o "[0-9]\{2\}\.[0-9]\{2\}")
+    ubuntu_version=$(cat /etc/lsb-release | grep -o "[0-9]\{2\}" | head -n 1)
   else
     echo "Unknown Linux Distro:"
     echo "$distro"
     echo "Aborting..."
     exit 1
   fi
+  echo "Distro: $distro"
+  version=$(cat /etc/lsb-release | grep -o "[0-9]\{2\}\.[0-9]\{2\}" | head -n 1)
+  echo "Version $version"
+  if [[ $ubuntu_version != 0 ]]; then
+    echo "Ubuntu derivative based on $ubuntu_version.x"
+  fi
 
-  if [[ ($distro == "Ubuntu" || $distro == "LinuxMint") && $ubuntu_version && $(echo "$ubuntu_version > 17.04" | bc) ]]; then
+  if [[ ($distro == "Ubuntu" || $distro == "LinuxMint") && $ubuntu_version && "$ubuntu_version" -lt "18" ]]; then
     echo "Older Ubuntu base detected. Adding ppas..."
     sudo add-apt-repository ppa:enlightenment-git/ppa -y
     sudo add-apt-repository ppa:neovim-ppa/stable -y
     echo "Done."
   fi
-  
+
   echo "Updating system repository database..."
   sudo apt-get update
   echo "Done."
 
   echo "Installing prerequisites..."
-  if [[ ($distro == "Ubuntu" || $distro == "LinuxMint") && $ubuntu_version && $(echo "$ubuntu_version > 17.04" | bc) ]]; then
+  sudo apt install curl -y
+  if [[ ($distro == "Ubuntu" || $distro == "LinuxMint") && $ubuntu_version && "$ubuntu_version" -lt "18" ]]; then
     sudo apt install terminology -y
+    cp terminology.cfg ~/.config/terminology/config/standard/base.cfg
   fi
-  
+
   sudo apt install neovim -y
   sudo apt install git -y
   sudo apt install fish -y
@@ -69,6 +94,7 @@ then
   python3 -m pip --user install setuptools
 
   # Node.js stuff
+  curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
   sudo apt install nodejs -y
   sudo npm install -g webpack
   sudo npm install -g webpack-cli
@@ -87,20 +113,9 @@ git config --global user.name jsmith
 git config --global user.email jasmith79@gmail.com
 git config --global push.default simple
 
-<<<<<<< HEAD
-# Install fira code font
-# Once installed will need to set in preferences in terminology.
-echo "done. Installing FiraCode font..."
-=======
-# Install terminology terminal emulator. "Official ubuntu repo is *very* outdated
-# Once installed set the theme to solarized in preferences.
-echo "done. Installing terminology..."
-sudo apt-get install terminology -y
-
 # Install fonts
 # Once installed will need to set in preferences in terminology.
 echo "done. Installing fonts..."
->>>>>>> 5bae2454c7a4da21fab7578176ac4e3bdb94b91b
 cd ~
 mkdir Fonts
 cd Fonts
@@ -133,12 +148,15 @@ python3 -m pip --user install neovim
 echo "done. Moving old configs to ~/.old_configs..."
 mkdir -p ~/.old_configs
 mv -t ~/.old_configs ~/.vimrc ~/.bashrc ~/.config/nvim ~/.config/fish/config.fish
+chown -R $user ~/.old_configs
 
 echo "done. Symlinking new configs..."
-mkdir -p ~/.config ~/.config/nvim
-ln -s ~/jasmith79-dotfiles/vimrc ~/.config/nvim/init.vim
-ln -s ~/jasmith79-dotfiles/bashrc ~/.bashrc
-ln -s ~/jasmith79-dotfiles/config.fish ~/.config/fish/config.fish
-ln -s ~/jasmith79-dotfiles/vimrc ~/.vimrc
-
+su - "$user" -c "mkdir -p ~/.config ~/.config/nvim"
+su - "$user" -c "mkdir -p ~/.config/fish"
+su - "$user" -c "ln -s ./vimrc ~/.config/nvim/init.vim"
+su - "$user" -c "ln -s ./bashrc ~/.bashrc"
+su - "$user" -c "ln -s ./config.fish ~/.config/fish/config.fish"
+su - "$user" -c "ln -s ./vimrc ~/.vimrc"
+echo "done. Sourcing copied .bashrc"
+source ~/.bashrc
 echo "done. Exiting."
