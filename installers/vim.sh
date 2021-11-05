5f2db39902af6ef38c24d99c1a6be187be0b4ac5
Jared Smith <jasmith79@gmail.com>
2021-11-05 14:32:52 -0400

    Fix typos

--- installers/vim.sh
+++ installers/vim.sh
@@ -8,7 +8,7 @@
 # This will grab the path to the dotfiles dir, quoting to
 # guard against spaces in paths, e.g. I set my user name to
 # "Jared Smith" or something.
-DOTFILES_DIR = "$(dirname "$(dirname "$(readlink -f "$0")")"
+DOTFILES_DIR = "$(dirname "$(dirname "$(readlink -f "$0")")")"
 
 # Determine current user
 user=$(logname)
@@ -27,7 +27,7 @@ if ! command -v vim > /dev/null; then
   if command -v brew > /dev/null; then
     sudo -u "$user" brew install vim
   elif command -v apt > /dev/null; then
-    sudo apt-get update && sudo apt install vim
+    sudo apt-get update && sudo apt install vim vim-doc vim-scripts ctags -y 
   else
     echo "Unrecognized platform, aborting vim install"
     exit 1
@@ -53,7 +53,8 @@ fi
 rm -f ~/.vimrc
 
 pushd $DOTFILES_DIR
-stow config
+stow -D vim
+stow vim
 popd
 
 echo "Done."
