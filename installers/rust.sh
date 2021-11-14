e9419353309a033e1c7262596bd300dccd2b36b3
Jared Smith <jasmith79@gmail.com>
2021-11-14 14:17:19 -0500

    Add rust lang setup

---
+++ installers/rust.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOTFILES_DIR="$(dirname "$(dirname "$(readlink -f "$0")")")"
+
+source "$DOTFILES_DIR/utils/install-pkg.sh"
+
+if ! command -v curl > /dev/null; then
+  install-pkg "curl"
+fi
+
+if ! command -v rustup > /dev/null; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  rustup install stable
+  rustup default stable
+fi
