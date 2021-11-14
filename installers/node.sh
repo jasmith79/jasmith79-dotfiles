c559e58cbd37bf6bf26fec55d1da31212d66bf19
Jared Smith <jasmith79@gmail.com>
2021-11-14 14:17:39 -0500

    Add nodejs setup

--- installers/node.sh
+++ installers/node.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-
+# Installs nodejs, npm, fnm, etc.
 DOTFILES_DIR="$(dirname "$(dirname "$(readlink -f "$0")")")"
 source "$DOTFILES_DIR/utils/install-pkg.sh"
+
+# Need rustup to install fnm
+source "$DOTFILES_DIR/installers/rust.sh"
+
+# Specify actual path because it may not be on $PATH yet
+~/.cargo/bin/cargo install fnm
+
+eval "$(fnm env)"
+
+if ! command -v node > /dev/null; then
+  install-pkg "nodejs"
+fi
+
+npm install -g yarn
+npm install -g ts-node
