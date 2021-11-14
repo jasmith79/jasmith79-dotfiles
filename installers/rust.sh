42634410d0b1fb9947597fde616edf5122fe1b97
Jared Smith <jasmith79@gmail.com>
2021-11-14 16:20:37 -0500

    Fix typo

--- installers/rust.sh
+++ installers/rust.sh
@@ -9,7 +9,7 @@ if ! command -v curl > /dev/null; then
 fi
 
 if ! command -v rustup > /dev/null; then
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- -y
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
   rustup install stable
   rustup default stable
 fi
