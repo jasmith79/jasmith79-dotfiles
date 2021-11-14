ed605e9d4b219f39b6842c2c645387e57911e6a5
Jared Smith <jasmith79@gmail.com>
2021-11-14 14:19:46 -0500

    Clean up, idempotency checks

--- installers/node.sh
+++ installers/node.sh
@@ -4,12 +4,16 @@ DOTFILES_DIR="$(dirname "$(dirname "$(readlink -f "$0")")")"
 source "$DOTFILES_DIR/utils/install-pkg.sh"
 
 # Need rustup to install fnm
-source "$DOTFILES_DIR/installers/rust.sh"
+if ! command -v rustup > /dev/null; then
+  source "$DOTFILES_DIR/installers/rust.sh"
+fi
 
-# Specify actual path because it may not be on $PATH yet
-~/.cargo/bin/cargo install fnm
-
-eval "$(fnm env)"
+if ! command -v fnm > /dev/null; then
+  # Specify actual path because it may not be on $PATH yet
+  # if it just got installed
+  ~/.cargo/bin/cargo install fnm
+  eval "$(fnm env)"
+fi
 
 if ! command -v node > /dev/null; then
   install-pkg "nodejs"
