aca900656fcdc98708c02e9a5eee2e9a8c80c797
Jared Smith <jasmith79@gmail.com>
2021-11-18 10:14:56 -0500

    Add macos specific install script

---
+++ installers/macos.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Installs the MacOS-specific parts of my setup
+
+if "$(uname)" != "Darwin"; then
+  echo "Trying to install MacOS components on another platform, aborting!" >&2
+  exit 1
+fi
+
+# Xcode CLI tools, e.g. git, clang
+xcode-select --install
+
+# Homebrew
+if ! command -v brew >/dev/null; then
+  sudo -u "$user" /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Current bash is 5.x but because of GPL licensing
+# issues Apple has MacOS bash pegged at *3.x* which
+# is now almost 15 years old. Installs if current
+# bash version < 5.0.0
+bash_version=$(bash --version | grep -oE '\d+\.\d+\.\d+')
+if [ "$bash_version" != "$(echo "$bash_version" "5.0.0" | sort -V | cut -f1 -d ' ')"]; then
+  brew install bash
+fi
+
+# I like having a consistent experience across OSes, and the GNU
+# utilities are usually better anyway.
+gnu_path="/usr/local/opt/coreutils/libexec/gnubin"
+case :$PATH:
+  in *:$gnu_path:*) ;;
+     *)
+      brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep
+      export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+      export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+      ;;
+esac
+
+has_iterm=$(ls /Applications | grep iTerm)
+if [ -z "$has_iterm" ]; then
+  has_iterm=$(ls $HOME/Applications | grep iTerm)
+fi
+
+if [ -z "$has_iterm" ]; then
+  brew install --cask iterm2
+fi
