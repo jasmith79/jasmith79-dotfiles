3fd5754345fb6e2d8c215bfe78b3f210f765dd30
Jared Smith <jasmith79@gmail.com>
2021-11-19 09:00:23 -0500

    Add java installation script

---
+++ installers/java.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DOTFILES_DIR="$(dirname "$(dirname "$(readlink -f "$0")")")"
+
+# NOTE: sourcing this ensures homebrew on macos
+source "$DOTFILES_DIR/utils/install-pkg.sh"
+source "$DOTFILES_DIR/utils/update-apt.sh"
+
+os=$(bash "$DOTFILES_DIR/utils/get-platform")
+
+if [[ "$os" =~ "ubuntu" ]]; then
+  update-apt
+  sudo apt install openjdk-8-jdk
+  sudo apt install openjdk-11-jdk
+elif [ "$os" = "macos" ]; then
+  brew tap adoptopenjdk/openjdk
+  brew cask install adoptopenjdk8
+  brew cask install adoptopenjdk11
+elif [ "$os" = "arch" ]; then
+  echo "Not implemented yet!" >&2
+  exit 1
+else
+  echo "Unsupported platform $os, aborting!" >&2
+  exit 1
+fi
+
+install-pkg maven
