ebd05604a62723975b4d542dbc6089f0f81d9968
Jared Smith <jasmith79@gmail.com>
2021-11-20 16:03:28 -0500

    Add misc installer

---
+++ installers/misc.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Installs various packages I use.
+dotfiles_dir="$(dirname "$(dirname "$(readlink -f "$0")")")"
+
+source "$dotfiles_dir/utils/install-pkg.sh"
+
+if ! command -v jq; then
+  install-pkg jq
+fi
+
+if ! command -v htop; then
+  install-pkg htop
+fi
+
+if ! command -v shellcheck; then
+  install-pkg shellcheck
+fi
+
+if ! command -v zip; then
+  install-pkg zip
+fi
+
+if ! command -v curl; then
+  install-pkg curl
+fi
+
+if ! command -v fzf; then
+  install-pkg fzf
+fi
+
+if ! command -v tree; then
+  install-pkg tree
+fi
+
+if ! command -v neofetch; then
+  install-pkg neofetch
+fi
+
+if ! command -v virtualbox; then
+  install-pkg virtualbox
+fi
