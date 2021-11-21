ba189e4f2eec2bc5880f6879c734ef18dc574cdb
Jared Smith <jasmith79@gmail.com>
2021-11-21 12:20:00 -0500

    Add idempotence checks

--- installers/linux.sh
+++ installers/linux.sh
@@ -12,7 +12,8 @@ fi
 
 if [[ "$os" =~ "ubuntu" ]]; then # includes mint
   update-apt
-  sudo apt install -y python3-pip
+
+  # I'm not going to bother writing conditionals for all of these.
   sudo apt install -y openssh-server
   sudo apt install -y virtualbox-qt
   sudo apt install -y whois
@@ -25,22 +26,33 @@ if [[ "$os" =~ "ubuntu" ]]; then # includes mint
   sudo apt install -y apt-transport-https
 
   # Dropbox
-  pushd ~ || exit 1
-  wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-  ~/.dropbox-dist
-  popd || exit 1
+  if ! [ -d "$HOME/Dropbox"]; then
+    pushd ~ || exit 1
+    wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+    ~/.dropbox-dist
+    popd || exit 1
+  fi
 
+  needs_update=false
   # Google chrome
-  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-  echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+  if ! command -v chrome > /dev/null; then
+    needs_update=true
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+  fi
 
   # Brave browser
-  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  if ! command -v brave > /dev/null; then
+    needs_update=true
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  fi
 
-  sudo apt-get update
-  sudo apt install -y google-chrome-stable
-  sudo apt install -y brave-browser
+  if [ "$needs_update" = true ]; then
+    sudo apt-get update
+    sudo apt install -y google-chrome-stable
+    sudo apt install -y brave-browser
+  fi
 
 elif [[ "$os" =~ "arch" ]]; then
   echo "Not yet implemented" >&2
