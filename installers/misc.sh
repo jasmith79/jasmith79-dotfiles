5bb893b6ea1782803e03b17706a56b6bb9cbf68a
Jared Smith <jasmith79@gmail.com>
2021-11-21 14:03:06 -0500

    Forgot to pipe output from check to /dev/null

--- installers/misc.sh
+++ installers/misc.sh
@@ -4,34 +4,34 @@ dotfiles_dir="$(dirname "$(dirname "$(readlink -f "$0")")")"
 
 source "$dotfiles_dir/utils/install-pkg.sh"
 
-if ! command -v jq; then
+if ! command -v jq> /dev/null; then
   install-pkg jq
 fi
 
-if ! command -v htop; then
+if ! command -v htop> /dev/null; then
   install-pkg htop
 fi
 
-if ! command -v shellcheck; then
+if ! command -v shellcheck> /dev/null; then
   install-pkg shellcheck
 fi
 
-if ! command -v zip; then
+if ! command -v zip> /dev/null; then
   install-pkg zip
 fi
 
-if ! command -v curl; then
+if ! command -v curl> /dev/null; then
   install-pkg curl
 fi
 
-if ! command -v fzf; then
+if ! command -v fzf> /dev/null; then
   install-pkg fzf
 fi
 
-if ! command -v tree; then
+if ! command -v tree> /dev/null; then
   install-pkg tree
 fi
 
-if ! command -v neofetch; then
+if ! command -v neofetch> /dev/null; then
   install-pkg neofetch
 fi
