d237af0a742087fe855c82671f28b73200faf7ea
Jared Smith <jasmith79@gmail.com>
2021-11-21 14:48:16 -0500

    Remove outdated cask install

--- utils/ensure-brew.sh
+++ utils/ensure-brew.sh
@@ -6,10 +6,4 @@ ensure-brew () {
   if ! command -v brew >/dev/null; then
     sudo -u "$user" /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
-
-  if ! brew info brew-cask &>/dev/null; then
-    brew tap homebrew/cask-versions
-    brew update
-    brew tap caskroom/cask
-  fi
 }
