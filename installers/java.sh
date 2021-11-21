9496c32c603dea3c3620b0c2d1a7bb5c2500d921
Jared Smith <jasmith79@gmail.com>
2021-11-21 06:41:51 -0500

    Add idempotence checks

--- installers/java.sh
+++ installers/java.sh
@@ -8,20 +8,24 @@ source "$dotfiles_dir/utils/update-apt.sh"
 
 os=$(bash "$dotfiles_dir/utils/get-platform")
 
-if [[ "$os" =~ "ubuntu" ]]; then
-  update-apt
-  sudo apt install openjdk-8-jdk
-  sudo apt install openjdk-11-jdk
-elif [ "$os" = "macos" ]; then
-  brew tap adoptopenjdk/openjdk
-  brew cask install adoptopenjdk8
-  brew cask install adoptopenjdk11
-elif [ "$os" = "arch" ]; then
-  echo "Not implemented yet!" >&2
-  exit 1
-else
-  echo "Unsupported platform $os, aborting!" >&2
-  exit 1
+if ! command -v javac > /dev/null; then
+  if [[ "$os" =~ "ubuntu" ]]; then
+    update-apt
+    sudo apt install openjdk-8-jdk
+    sudo apt install openjdk-11-jdk
+  elif [ "$os" = "macos" ]; then
+    brew tap adoptopenjdk/openjdk
+    brew cask install adoptopenjdk8
+    brew cask install adoptopenjdk11
+  elif [ "$os" = "arch" ]; then
+    echo "Not implemented yet!" >&2
+    exit 1
+  else
+    echo "Unsupported platform $os, aborting!" >&2
+    exit 1
+  fi
 fi
 
-install-pkg maven
+if ! command -v mvn > /dev/null; then
+  install-pkg maven
+fi
