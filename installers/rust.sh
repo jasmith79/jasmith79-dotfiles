25ccabaee46ce9b394da0942c8b49a3deef03f57
Jared Smith <jasmith79@gmail.com>
2022-06-06 07:32:39 -0400

    Add rust analyzer install

--- installers/rust.sh
+++ installers/rust.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 dotfiles_dir="$(dirname "$(dirname "$(readlink -f "$0")")")"
+os=$(bash "$dotfiles_dir/utils/get-platform") || exit 1
 
 source "$dotfiles_dir/utils/install-pkg.sh"
+source "$dotfiles_dir/utils/pushpop.sh"
 
 # Need e.g. build-essential on Linux, easier to
 # just grab all my C/C++ setup.
@@ -16,7 +18,27 @@ fi
 
 if ! command -v rustup > /dev/null; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-  source $HOME/.cargo/env
+  source "$HOME"/.cargo/env
   rustup install stable
   rustup default stable
 fi
+
+if ! command -v rust-analyzer > /dev/null; then
+  pushd /tmp
+  if [[ "$os" =~ "mac" ]]; then
+    RUST_ANALYZER_LINK="https://github.com/rust-lang/rust-analyzer/releases/download/2022-05-30/rust-analyzer-x86_64-apple-darwin.gz"
+  else
+    RUST_ANALYZER_LINK="https://github.com/rust-lang/rust-analyzer/releases/download/2022-06-06/rust-analyzer-x86_64-unknown-linux-gnu.gz"
+  fi
+
+  if [ -z "$RUST_ANALYZER_LINK" ]; then
+    exit 1
+  else
+    curl -O "$RUST_ANALYZER_LINK"
+    gzip -d rust-analyzer-x86_64-apple-darwin.gz
+    chmod +x rust-analyzer-x86_64-apple-darwin
+    mv ./rust-analyzer-x86_64-apple-darwin /usr/local/bin/rust-analyzer
+  fi
+  popd
+fi
+
