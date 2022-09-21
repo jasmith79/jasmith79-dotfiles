a1d6d66d976f53a52e024c91550dbaf29d44dee5
jsmith <jasmith79@gmail.com>
2022-09-21 14:18:24 -0400

    Renable gitsigns

--- nvim/.config/nvim/lua/jsmith/packer.lua
+++ nvim/.config/nvim/lua/jsmith/packer.lua
@@ -32,12 +32,12 @@ return packer.startup(function(use)
     "nvim-lualine/lualine.nvim",
     requires = { "kyazdani42/nvim-web-devicons", opt = true }
   }
-  -- use {
-	--   "lewis6991/gitsigns.nvim",
-	--   config = function()
-	-- 	  require("gitsigns").setup()
-	--   end
-  -- }
+  use {
+	  "lewis6991/gitsigns.nvim",
+	  config = function()
+		  require("gitsigns").setup()
+	  end
+  }
   use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
   use {
 	  'TimUntersberger/neogit',
