0d115eb87b34d49b08abb7363557f7be4d2c3931
jsmith <jasmith79@gmail.com>
2023-05-06 14:05:10 -0400

    chore: remove old terminology stuffs, switched to wezterm

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -23,19 +23,6 @@ end
 # end
 fish_vi_key_bindings
 
-# If using terminology, create an appropriate alias and set transparency to 80
-# term_emu="/"(ps -f -p (cat /proc/(echo %self)/stat | cut -d \  -f 4) | tail -1 | sed 's/^.* //'
-# if $term_emu =~ "terminology"
-#   # The fullscreen option doesn't work in Cinnamon. 150 col x 55 lines is a
-#   # reasonably large starting geometry, can max with mouse or keyboard after
-#   # opening.
-#   alias bigt='terminology -g 150x75 -S v-h'
-#   tyalpha 80
-#   function newt
-#     terminology $argv > /dev/null ^&1
-#   end
-# end
-
 set SPACEFISH_USER_SHOW always
 set SPACEFISH_USER_COLOR af5fff
 set SPACEFISH_DIR_COLOR ffaf5f
