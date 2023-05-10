1060b3933db4b57f8989546dcdc5c27677e18291
jsmith <jasmith79@gmail.com>
2023-05-10 09:18:07 -0400

    chore: disable checkThirdParty check in lua config

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -3,28 +3,28 @@ local Remap = require('jsmith.keymap')
 
 cmp.setup({
     snippet = {
-      expand = function(args)
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      end,
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
     },
     window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-      ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-      ['<Tab>'] = cmp.mapping.select_next_item(),
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' }, -- For luasnip users.
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, -- For luasnip users.
     }, {
-      { name = 'buffer' },
+        { name = 'buffer' },
     })
 })
 
@@ -57,21 +57,21 @@ cmp.setup.cmdline(':', {
 
 -- Thanks prime!
 local function config(_config)
-	return vim.tbl_deep_extend("force", {
-		capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-		on_attach = function()
-			Remap.nnoremap("gd", function() vim.lsp.buf.definition() end)
-			Remap.nnoremap("K", function() vim.lsp.buf.hover() end)
-			Remap.nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
-			Remap.nnoremap("<leader>vd", function() vim.diagnostic.open_float() end)
-			Remap.nnoremap("[d", function() vim.diagnostic.goto_next() end)
-			Remap.nnoremap("]d", function() vim.diagnostic.goto_prev() end)
-			Remap.nnoremap("<leader>vca", function() vim.lsp.buf.code_action() end)
-			Remap.nnoremap("<leader>vrr", function() vim.lsp.buf.references() end)
-			Remap.nnoremap("<leader>vrn", function() vim.lsp.buf.rename() end)
-			Remap.inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
-		end,
-	}, _config or {})
+    return vim.tbl_deep_extend("force", {
+        capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+        on_attach = function()
+            Remap.nnoremap("gd", function() vim.lsp.buf.definition() end)
+            Remap.nnoremap("K", function() vim.lsp.buf.hover() end)
+            Remap.nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+            Remap.nnoremap("<leader>vd", function() vim.diagnostic.open_float() end)
+            Remap.nnoremap("[d", function() vim.diagnostic.goto_next() end)
+            Remap.nnoremap("]d", function() vim.diagnostic.goto_prev() end)
+            Remap.nnoremap("<leader>vca", function() vim.lsp.buf.code_action() end)
+            Remap.nnoremap("<leader>vrr", function() vim.lsp.buf.references() end)
+            Remap.nnoremap("<leader>vrn", function() vim.lsp.buf.rename() end)
+            Remap.inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
+        end,
+    }, _config or {})
 end
 
 local lspconfig = require("lspconfig")
@@ -85,7 +85,7 @@ lspconfig.rust_analyzer.setup(config({
                     group = "module",
                 },
                 prefix = "self",
-           },
+            },
             cargo = {
                 buildScripts = {
                     enable = true,
@@ -99,26 +99,27 @@ lspconfig.rust_analyzer.setup(config({
 }))
 
 lspconfig.lua_ls.setup(config({
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
     },
-  },
 }))
 
 lspconfig.bashls.setup(config())
@@ -127,17 +128,16 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 lspconfig.cssls.setup(config({
-  capabilities = capabilities,
+    capabilities = capabilities,
 }))
 
 lspconfig.hls.setup(config({
-  settings = {
- 	haskell = {
-      hlintOn = true,
-	  formattingProvider = "fourmolu"
-	}
-  }
+    settings = {
+        haskell = {
+            hlintOn = true,
+            formattingProvider = "fourmolu"
+        }
+    }
 }))
 
 vim.g.neoformat_try_node_exe = 1
-
