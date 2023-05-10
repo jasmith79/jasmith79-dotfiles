a125d66d1e176ace61e231adb1737923583e67b2
jsmith <jasmith79@gmail.com>
2023-05-10 18:21:55 -0400

    fix: change telescope to use git_files

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,6 @@
 local Remap = require("jsmith.keymap")
-Remap.nnoremap("<leader>ff", "<cmd>Telescope find_files<cr>")
+Remap.nnoremap("<leader>ff", "<cmd>Telescope git_files<cr>")
 Remap.nnoremap("<leader>fw", "<cmd>Telescope live_grep<cr>")
 Remap.nnoremap("<leader>fb", "<cmd>Telescope buffers<cr>")
 Remap.nnoremap("<leader>fh", "<cmd>Telescope help_tags<cr>")
 Remap.nnoremap("<leader>fr", "<cmd>Telescope oldfiles<cr>")
-
